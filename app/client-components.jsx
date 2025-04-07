508ed8b0015490b866d93be44fd8928fe1aebeaa
shreyansh1607 <shreyansh2210010@akgec.ac.in>
2025-04-08 00:19:10 +0530

    server -client side division in layout

---
+++ app/client-components.jsx
@@ -0,0 +1,22 @@
+// app/client-components.jsx (CLIENT COMPONENT)
+"use client";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/footer";
+import ScrollToTop from "./components/helper/scroll-to-top";
+import Navbar from "./components/navbar";
+import "./css/card.scss";
+
+export default function ClientComponents({ children }) {
+  return (
+    <>
+      <ToastContainer />
+      <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+        <Navbar />
+        {children}
+        <ScrollToTop />
+      </main>
+      <Footer />
+    </>
+  );
+}
