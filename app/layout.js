41cea8eba7724c1dbf5a52ec11442766409834f2
shreyansh1607 <shreyansh2210010@akgec.ac.in>
2025-04-08 00:10:32 +0530

    meta data hata

--- app/layout.js
+++ app/layout.js
@@ -10,13 +10,14 @@ import "./css/card.scss";
 import "./css/globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Portfolio of shreyansh yadav - Software Developer",
-  description:
-    "This is the portfolio of Shreyansh. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
-};
+// export const metadata = {
+//   title: "Portfolio of shreyansh yadav - Software Developer",
+//   description:
+//     "This is the portfolio of Shreyansh. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+// };
 
-export default function RootLayout({ children }) {
+export default function RootLayout
+({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
